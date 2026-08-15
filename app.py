d7f4672599a68f61dae6160f4036db84df525336
GabrielOliveiraFecap <gabriel.oliveira4@edu.fecap.br>
2026-08-15 12:19:49 -0300

    hover legal adicionado, .env tambem

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+import os 
 import requests
+from dotenv import load_dotenv
 
-token = ""
+load_dotenv()
+
+'''token = os.getenv("TOKEN")
 response = requests.get(
     "https://brapi.dev/api/v2/stocks/quote",
     headers={"Authorization": f"Bearer {token}"},
@@ -13,20 +17,21 @@ for item in data["results"]:
     quote = item["data"]
     print(item["symbol"], quote["regularMarketPrice"])
 
-'''apiKey = ""
+
+
 urlBase = "https://brapi.dev/api/v2/stocks/quote?symbols=PETR4,VALE3,ITUB4,MGLU3"
 parametros = {
-    'key': apiKey,
+    headers={"Authorization": f"Bearer {token}"}
 }
 reposta = requests.get(urlBase, params=parametros)
 
 print(reposta.content)'''
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-#@app.route("/")
-#def home():
- #   return render_template("index.html", titulo="B3 Tracker")
+@app.route("/")
+def home():
+   return render_template("index.html", titulo="B3 Tracker")
 
-#if __name__ == "__main__":
- #   app.run(debug=True)
+if __name__ == "__main__":
+   app.run(debug=True)
